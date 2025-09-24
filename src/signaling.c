c1b97e8071550121719937658ccfb8668535f434
Timothy Felt <timothyfelt@MacBook-Pro-7.local>
2025-09-24 13:12:08 -0600

    work in progress.

--- src/signaling.c
+++ src/signaling.c
@@ -1,8 +1,11 @@
 #include "signaling.h"
 
-void signal_handle_calculation(SemaphoreHandle_t request,
-                               SemaphoreHandle_t response,
-                               struct signal_data *data);
+void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data);
+{
+
+printf("taken the sempahore. Performing the calculation");
+data->output = data->input + 5;
+}
 
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,

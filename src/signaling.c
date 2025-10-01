69c056a8145fb6967fc1f7f988c7f63a624e531f
BrianStites <bsti7375@yahoo.com>
2025-10-01 13:12:58 -0600

    Updated cmake to build test_signal.c.

--- src/signaling.c
+++ src/signaling.c
@@ -1,15 +1,16 @@
+#include <stdio.h>
 #include "signaling.h"
 
-void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data);
+void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
-    printf("taken the sempahore. Performing the calculation");
+    printf("taken the sempahore. Performing the calculation\n");
     xSemaphoreTake(request, 100);
     data->output = data->input + 5;
     vTaskDelay(100);
     xSemaphoreGive(response);
 }
 
-BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data);
+BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
     xSemaphoreGive(request);
     BaseType_t res = xSemaphoreTake(response, 100);

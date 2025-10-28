10787875ae60334f546fb343046d600a374d0c40
BrianStites <bsti7375@yahoo.com>
2025-10-28 09:41:34 -0600

    Added a README file in the test folder that contains the test results.

--- src/signaling.c
+++ src/signaling.c
@@ -3,7 +3,7 @@
 
 void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
-    printf("taken the sempahore. Performing the calculation\n");
+    //printf("taken the sempahore. Performing the calculation\n");
     xSemaphoreTake(request, portMAX_DELAY);
     data->output = data->input + 5;
     //vTaskDelay(100);
